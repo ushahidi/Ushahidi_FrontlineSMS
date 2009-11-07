e9a1ee9ace4c88a3e52ba0826a30db6c5d1f2038
Emmanuel Kala <ekala@trinity.(none)>
2009-11-07 12:25:25 +0300

    Removed smslib; to be checked in a fresh

--- src/main/java/org/smslib/UnrecognizedHandlerProtocolException.java
+++
@@ -1,8 +0,0 @@
-package org.smslib;
-
-@SuppressWarnings("serial")
-public class UnrecognizedHandlerProtocolException extends SMSLibDeviceException {
-	public UnrecognizedHandlerProtocolException(int protocol) {
-		super("Unrecognized message protocol: " + protocol);
-	}
-}
