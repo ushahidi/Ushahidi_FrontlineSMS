0bbbeec94f4303223f23aecc8d73513f53f2694b
Brian Muita <bmuita@gmail.com>
2011-01-14 12:26:19 +0300

    Rename getI18NString to getI18nString

--- src/main/java/com/ushahidi/plugins/mapping/util/MappingMessages.java
+++ src/main/java/com/ushahidi/plugins/mapping/util/MappingMessages.java
@@ -1,6 +1,6 @@
 package com.ushahidi.plugins.mapping.util;
 
-import static net.frontlinesms.ui.i18n.InternationalisationUtils.getI18NString;
+import static net.frontlinesms.ui.i18n.InternationalisationUtils.getI18nString;
 
 /**
  * MappingMessages
@@ -10,291 +10,291 @@ import static net.frontlinesms.ui.i18n.InternationalisationUtils.getI18NString;
 public final class MappingMessages {
 	
 	public static String getYes() {
-		return getI18NString("plugins.mapping.yes");
+		return getI18nString("plugins.mapping.yes");
 	}
 	
 	public static String getNo() {
-		return getI18NString("plugins.mapping.no");
+		return getI18nString("plugins.mapping.no");
 	}
 	
 	public static String getMessageReceived() {
-		return getI18NString("plugins.mapping.message.received");
+		return getI18nString("plugins.mapping.message.received");
 	}
 	
 	public static String getFormResponse() {
-		return getI18NString("plugins.mapping.form.response");
+		return getI18nString("plugins.mapping.form.response");
 	}
 	
 	public static String getTextFormResponse() {
-		return getI18NString("plugins.mapping.textform.response");
+		return getI18nString("plugins.mapping.textform.response");
 	}
 	
 	public static String getIncidentReport() {
-		return getI18NString("plugins.mapping.report.report");
+		return getI18nString("plugins.mapping.report.report");
 	}
 	
 	public static String getTitle() {
-		return getI18NString("plugins.mapping.report.title");
+		return getI18nString("plugins.mapping.report.title");
 	}
 
 	public static String getTitleInfo() {
-		return getI18NString("plugins.mapping.report.title.info");
+		return getI18nString("plugins.mapping.report.title.info");
 	}
 	
 	public static String getTitleKeyword() {
-		return getI18NString("plugins.mapping.report.title.keyword");
+		return getI18nString("plugins.mapping.report.title.keyword");
 	}
 	
 	public static String getDescription() {
-		return getI18NString("plugins.mapping.report.description");
+		return getI18nString("plugins.mapping.report.description");
 	}
 
 	public static String getDescriptionInfo() {
-		return getI18NString("plugins.mapping.report.description.info");
+		return getI18nString("plugins.mapping.report.description.info");
 	}
 	
 	public static String getDescriptionKeyword() {
-		return getI18NString("plugins.mapping.report.description.keyword");
+		return getI18nString("plugins.mapping.report.description.keyword");
 	}
 	
 	public static String getDate() {
-		return getI18NString("plugins.mapping.report.date");
+		return getI18nString("plugins.mapping.report.date");
 	}
 	
 	public static String getDateInfo() {
-		return getI18NString("plugins.mapping.report.date.info");
+		return getI18nString("plugins.mapping.report.date.info");
 	}
 	
 	public static String getDateKeyword() {
-		return getI18NString("plugins.mapping.report.date.keyword");
+		return getI18nString("plugins.mapping.report.date.keyword");
 	}
 	
 	public static String getDateInvalid() {
-		return getI18NString("plugins.mapping.report.date.invalid");
+		return getI18nString("plugins.mapping.report.date.invalid");
 	}
 
 	public static String getDateNow() {
-		return getI18NString("plugins.mapping.report.datenow");
+		return getI18nString("plugins.mapping.report.datenow");
 	}
 	
 	public static String getDateNowInfo() {
-		return getI18NString("plugins.mapping.report.datenow.info");
+		return getI18nString("plugins.mapping.report.datenow.info");
 	}
 	
 	public static String getDateNowKeyword() {
-		return getI18NString("plugins.mapping.report.datenow.keyword");
+		return getI18nString("plugins.mapping.report.datenow.keyword");
 	}
 	
 	public static String getTime() {
-		return getI18NString("plugins.mapping.report.time");
+		return getI18nString("plugins.mapping.report.time");
 	}
 	
 	public static String getLocation() {
-		return getI18NString("plugins.mapping.report.location");
+		return getI18nString("plugins.mapping.report.location");
 	}
 	
 	public static String getLocationOther() {
-		return getI18NString("plugins.mapping.report.location.other");
+		return getI18nString("plugins.mapping.report.location.other");
 	}
 	
 	public static String getLocationInfo() {
-		return getI18NString("plugins.mapping.report.location.info");
+		return getI18nString("plugins.mapping.report.location.info");
 	}
 	
 	public static String getLocationKeyword() {
-		return getI18NString("plugins.mapping.report.location.keyword");
+		return getI18nString("plugins.mapping.report.location.keyword");
 	}
 	
 	public static String getDefaultLocation() {
-		return getI18NString("plugins.mapping.report.default.location");
+		return getI18nString("plugins.mapping.report.default.location");
 	}
 
 	public static String getDefaultLocationInfo() {
-		return getI18NString("plugins.mapping.report.default.location.info");
+		return getI18nString("plugins.mapping.report.default.location.info");
 	}
 	
 	public static String getDefaultLocationKeyword() {
-		return getI18NString("plugins.mapping.report.default.location.keyword");
+		return getI18nString("plugins.mapping.report.default.location.keyword");
 	}
 	
 	public static String getCategories() {
-		return getI18NString("plugins.mapping.report.categories");
+		return getI18nString("plugins.mapping.report.categories");
 	}
 	
 	public static String getCategoriesInfo() {
-		return getI18NString("plugins.mapping.report.categories.info");
+		return getI18nString("plugins.mapping.report.categories.info");
 	}
 	
 	public static String getCategoriesKeyword() {
-		return getI18NString("plugins.mapping.report.categories.keyword");
+		return getI18nString("plugins.mapping.report.categories.keyword");
 	}
 	
 	public static String getFirstName() {
-		return getI18NString("plugins.mapping.report.firstname");
+		return getI18nString("plugins.mapping.report.firstname");
 	}
 
 	public static String getFirstNameInfo() {
-		return getI18NString("plugins.mapping.report.firstname.info");
+		return getI18nString("plugins.mapping.report.firstname.info");
 	}
 	
 	public static String getFirstNameKeyword() {
-		return getI18NString("plugins.mapping.report.firstname.keyword");
+		return getI18nString("plugins.mapping.report.firstname.keyword");
 	}
 	
 	public static String getLastName() {
-		return getI18NString("plugins.mapping.report.lastname");
+		return getI18nString("plugins.mapping.report.lastname");
 	}
 	
 	public static String getLastNameInfo() {
-		return getI18NString("plugins.mapping.report.lastname.info");
+		return getI18nString("plugins.mapping.report.lastname.info");
 	}
 	
 	public static String getLastNameKeyword() {
-		return getI18NString("plugins.mapping.report.lastname.keyword");
+		return getI18nString("plugins.mapping.report.lastname.keyword");
 	}
 	
 	public static String getEmail() {
-		return getI18NString("plugins.mapping.report.email");
+		return getI18nString("plugins.mapping.report.email");
 	}
 	
 	public static String getEmailInfo() {
-		return getI18NString("plugins.mapping.report.email.info");
+		return getI18nString("plugins.mapping.report.email.info");
 	}
 	
 	public static String getEmailKeyword() {
-		return getI18NString("plugins.mapping.report.email.keyword");
+		return getI18nString("plugins.mapping.report.email.keyword");
 	}
 	
 	public static String getNews() {
-		return getI18NString("plugins.mapping.report.news");
+		return getI18nString("plugins.mapping.report.news");
 	}
 	
 	public static String getNewsInfo() {
-		return getI18NString("plugins.mapping.report.news.info");
+		return getI18nString("plugins.mapping.report.news.info");
 	}
 	
 	public static String getNewsKeyword() {
-		return getI18NString("plugins.mapping.report.news.keyword");
+		return getI18nString("plugins.mapping.report.news.keyword");
 	}
 	
 	public static String getVideo() {
-		return getI18NString("plugins.mapping.report.video");
+		return getI18nString("plugins.mapping.report.video");
 	}
 	
 	public static String getVideoInfo() {
-		return getI18NString("plugins.mapping.report.video.info");
+		return getI18nString("plugins.mapping.report.video.info");
 	}
 	
 	public static String getVideoKeyword() {
-		return getI18NString("plugins.mapping.report.video.keyword");
+		return getI18nString("plugins.mapping.report.video.keyword");
 	}
 	
 	public static String getFormCreated() {
-		return getI18NString("plugins.mapping.setup.forms.created");
+		return getI18nString("plugins.mapping.setup.forms.created");
 	}
 	
 	public static String getFormFailed() {
-		return getI18NString("plugins.mapping.setup.forms.failed");
+		return getI18nString("plugins.mapping.setup.forms.failed");
 	}
 	
 	public static String getTextFormCreated() {
-		return getI18NString("plugins.mapping.setup.textforms.created");
+		return getI18nString("plugins.mapping.setup.textforms.created");
 	}
 	
 	public static String getTextFormFailed() {
-		return getI18NString("plugins.mapping.setup.textforms.failed");
+		return getI18nString("plugins.mapping.setup.textforms.failed");
 	}
 	
 	public static String getSourceNameMissing() {
-		return getI18NString("plugins.mapping.setup.sourcename.missing");
+		return getI18nString("plugins.mapping.setup.sourcename.missing");
 	}
 	
 	public static String getSourceUrlMissing() {
-		return getI18NString("plugins.mapping.setup.source.missing");
+		return getI18nString("plugins.mapping.setup.source.missing");
 	}
 	
 	public static String getSetupDefaultMissing() {
-		return getI18NString("plugins.mapping.setup.default.missing");
+		return getI18nString("plugins.mapping.setup.default.missing");
 	}
 	
 	public static String getSetupDefaultRequired() {
-		return getI18NString("plugins.mapping.setup.default.required");
+		return getI18nString("plugins.mapping.setup.default.required");
 	}
 	
 	public static String getSetupMappingExists() {
-		return getI18NString("plugins.mapping.setup.mapping.exists");
+		return getI18nString("plugins.mapping.setup.mapping.exists");
 	}
 	
 	public static String getSetupSynchronize() {
-		return getI18NString("plugins.mapping.setup.synchronize");
+		return getI18nString("plugins.mapping.setup.synchronize");
 	}
 	
 	public static String getAllCategories() {
-		return getI18NString("plugins.mapping.categories.all");
+		return getI18nString("plugins.mapping.categories.all");
 	}
 	
 	public static String getErrorCreatingIncident() {
-		return getI18NString("plugins.mapping.report.error.creating");
+		return getI18nString("plugins.mapping.report.error.creating");
 	}
 	
 	public static String getErrorUpdatingIncident() {
-		return getI18NString("plugins.mapping.report.error.updating");
+		return getI18nString("plugins.mapping.report.error.updating");
 	}
 	
 	public static String getErrorInvalidLocation() {
-		return getI18NString("plugins.mapping.report.error.location");
+		return getI18nString("plugins.mapping.report.error.location");
 	}
 	
 	public static String getSearchMessages() {
-		return getI18NString("plugins.mapping.search.messages");
+		return getI18nString("plugins.mapping.search.messages");
 	}
 	
 	public static String getSearchIncidents() {
-		return getI18NString("plugins.mapping.search.incidents");
+		return getI18nString("plugins.mapping.search.incidents");
 	}
 	
 	public static String getSearchContacts() {
-		return getI18NString("plugins.mapping.search.contacts");
+		return getI18nString("plugins.mapping.search.contacts");
 	}
 	
 	public static String getIncidentCreatedFromForm() {
-		return getI18NString("plugins.mapping.report.created.via.form");
+		return getI18nString("plugins.mapping.report.created.via.form");
 	}
 	
 	public static String getIncidentCreatedFromTextForm() {
-		return getI18NString("plugins.mapping.report.created.via.textform");
+		return getI18nString("plugins.mapping.report.created.via.textform");
 	}
 	
 	public static String getIncidentUpdatedFromTextForm() {
-		return getI18NString("plugins.mapping.report.updated.via.textform");
+		return getI18nString("plugins.mapping.report.updated.via.textform");
 	}
 	
 	public static String getIncidentAddPhoto() {
-		return getI18NString("plugins.mapping.report.add.photo");
+		return getI18nString("plugins.mapping.report.add.photo");
 	}
 	
 	public static String getSelectFile() {
-		return getI18NString("plugins.mapping.select.file");
+		return getI18nString("plugins.mapping.select.file");
 	}
 	
 	public static String getMapCacheSize() {
-		return getI18NString("plugins.mapping.setup.cache.size");
+		return getI18nString("plugins.mapping.setup.cache.size");
 	}
 	
 	public static String getMapCacheFilesDeleted() {
-		return getI18NString("plugins.mapping.setup.cache.files.deleted");
+		return getI18nString("plugins.mapping.setup.cache.files.deleted");
 	}
 	
 	public static String getWarningFormsMissing() {
-		return getI18NString("plugins.mapping.warning.missing.forms");
+		return getI18nString("plugins.mapping.warning.missing.forms");
 	}
 	
 	public static String getWarningTextFormsMissing() {
-		return getI18NString("plugins.mapping.warning.missing.textforms");
+		return getI18nString("plugins.mapping.warning.missing.textforms");
 	}
 	
 	public static String getWarningFormsAndTextFormsMissing() {
-		return getI18NString("plugins.mapping.warning.missing.forms.and.textforms");
+		return getI18nString("plugins.mapping.warning.missing.forms.and.textforms");
 	}
 	
 }
